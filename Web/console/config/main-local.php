ffca277948b348da3d55f6427255c19c4c64cdc7
maxdev <maxim.dudai01@gmail.com>
2024-11-16 20:41:00 +0000

    GIT ignored files

---
+++ Web/console/config/main-local.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'bootstrap' => ['gii'],
+    'components' => [
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+        ],
+    ],
+    'modules' => [
+        'gii' => 'yii\gii\Module',
+    ],
+
+];
