b86c176e53e818ddc7ace075736150df5efe4b80
maxdev <maxim.dudai01@gmail.com>
2024-12-10 21:49:43 +0000

    PFW-15
    - created new package for constants variables used for sharedPreferences
    - removing sharedPreferences data when user use logout

--- Mobile/app/src/main/java/pt/ipleiria/estg/dei/fastwheels/constants/Constants.java
+++ Mobile/app/src/main/java/pt/ipleiria/estg/dei/fastwheels/constants/Constants.java
@@ -1,2 +1,10 @@
-package pt.ipleiria.estg.dei.fastwheels.constants;public class Constants {
+package pt.ipleiria.estg.dei.fastwheels.constants;
+
+
+public class Constants {
+    public static final String PREFS_NAME = "FastWheelsPrefs";
+    public static final String KEY_EMAIL = "email";
+    public static final String KEY_PASSWORD = "password";
+    public static final String KEY_KEEP_LOGGED_IN = "keepLoggedIn";
 }
+
