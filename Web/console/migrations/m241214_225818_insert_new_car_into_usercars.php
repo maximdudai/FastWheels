75846ee247a4f31c0059c3ba3b2403615e635850
maxdev <maxim.dudai01@gmail.com>
2024-12-21 00:09:27 +0000

    update db table name userCars

--- Web/console/migrations/m241214_225818_insert_new_car_into_usercars.php
+++ Web/console/migrations/m241214_225818_insert_new_car_into_usercars.php
@@ -13,25 +13,25 @@ class m241214_225818_insert_new_car_into_usercars extends Migration
      */
     public function safeUp()
     {
-        $clientId = Client::findOne(['name' => 'maximdudai']) -> userId;
+        // $clientId = Client::findOne(['name' => 'maximdudai']) -> userId;
 
-        if(!$clientId) {
-            echo "Client not found";
-            return false;
-        }
+        // if(!$clientId) {
+        //     echo "Client not found";
+        //     return false;
+        // }
 
-        $this->insert('usercars', [
-            'id' => 1, 
-            'clientId' => $clientId,
-            'carBrand' => 'Toyota Corolla',
-            'carModel' => 'LE', 
-            'carYear' => 2020,
-            'carDoors' => 4,
-            'createdAt' => date('Y-m-d H:i:s'), 
-            'status' => 'available',
-            'availableFrom' => '2024-01-01', 
-            'availableTo' => '2024-12-31',
-        ]);
+        // $this->insert('usercars', [
+        //     'id' => 1, 
+        //     'clientId' => $clientId,
+        //     'carBrand' => 'Toyota Corolla',
+        //     'carModel' => 'LE', 
+        //     'carYear' => 2020,
+        //     'carDoors' => 4,
+        //     'createdAt' => date('Y-m-d H:i:s'), 
+        //     'status' => 'available',
+        //     'availableFrom' => '2024-01-01', 
+        //     'availableTo' => '2024-12-31',
+        // ]);
     }
 
     /**
