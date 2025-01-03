28fd68f4781aec9b83d45677be3dbb8059319e83
maxdev <maxim.dudai01@gmail.com>
2025-01-03 23:07:23 +0000

    created at

--- Web/console/migrations/m250103_230230_add_admin_user.php
+++ Web/console/migrations/m250103_230230_add_admin_user.php
@@ -30,7 +30,7 @@ class m250103_230230_add_admin_user extends Migration
             'email' => 'admin@example.com',
             'phone' => 'none',
             'roleId' => 3,
-            'createdAt' => time(),
+            'createdAt' => Date('Y-m-d H:i:s'),
             'balance' => 0,
             'iban' => 'none',
             'userId' => 1,
